4478aca8be577dd94bc82c27b33d46b628f386ab
DiegoCastro7 <137318162+DiegoCastro7@users.noreply.github.com>
2023-11-23 11:35:31 -0500

    feat: :sparkles: Controllers

---
+++ API/Controllers/DetallePedidoController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+public class DetallePedidoController: BaseController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public DetallePedidoController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<DetallePedido>>> Get()
+        {
+            var entidades = await _unitOfWork.DetallePedidos.GetAllAsync();
+            return _mapper.Map<List<DetallePedido>>(entidades);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<DetallePedidoDto>> Get(int id)
+        {
+            var entidad = await _unitOfWork.DetallePedidos.GetByIdAsync(id);
+            if(entidad == null)
+            {
+                return NotFound();
+            }
+            return _mapper.Map<DetallePedidoDto>(entidad);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<DetallePedido>> Post(DetallePedidoDto DetallePedidoDto)
+        {
+            var entidad = _mapper.Map<DetallePedido>(DetallePedidoDto);
+            this._unitOfWork.DetallePedidos.Add(entidad);
+            await _unitOfWork.SaveAsync();
+            if(entidad == null)
+            {
+                return BadRequest();
+            }
+            DetallePedidoDto.Id = entidad.Id;
+            return CreatedAtAction(nameof(Post), new {id = DetallePedidoDto.Id}, DetallePedidoDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<DetallePedidoDto>> Put(int id, [FromBody] DetallePedidoDto DetallePedidoDto)
+        {
+            if(DetallePedidoDto == null)
+            {
+                return NotFound();
+            }
+            var entidades = _mapper.Map<DetallePedido>(DetallePedidoDto);
+            _unitOfWork.DetallePedidos.Update(entidades);
+            await _unitOfWork.SaveAsync();
+            return DetallePedidoDto;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var entidad = await _unitOfWork.DetallePedidos.GetByIdAsync(id);
+            if(entidad == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.DetallePedidos.Delete(entidad);
+            await _unitOfWork.SaveAsync();
+            return NoContent();
+        }
+    }

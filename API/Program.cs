6dd637ca244baf5abd6ce267d2ab91a00d53babf
DiegoCastro7 <137318162+DiegoCastro7@users.noreply.github.com>
2023-11-23 12:21:24 -0500

    feat: :sparkles: Other Configurations Project

--- API/Program.cs
+++ API/Program.cs
@@ -1,27 +1,38 @@
 //dotnet ef dbcontext scaffold "server=localhost;user=root;password=123456;database=garden" Pomelo.EntityFrameworkCore.MySql -s API -p Persistence --context WebApiContext --context-dir Data --output-dir Entities
 
+using System.Reflection;
+using API.Extension;
+using AspNetCoreRateLimit;
+using Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.ConfigureRateLimiting();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.ConfigureCors();
+builder.Services.AddAplicationServices();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<GardenContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
-
+app.UseIpRateLimiting();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
